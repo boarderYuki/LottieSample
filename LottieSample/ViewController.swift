98854ea268ae7c03bafee72619de7b4bc35fb9bd
yuki.pro <yuki.kr@gmail.com>
2018-07-03 00:25:26 +0900

    no message

--- LottieSample/ViewController.swift
+++ LottieSample/ViewController.swift
@@ -15,20 +15,69 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var aniView: UIView!
     
+    var menuOn = false
+    var hamburgerMenuButton:LOTAnimationView?
+    let hamburgerMenuFrame = CGRect(x: 0, y: 10, width: 75, height: 75)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     
+        addHamburgerMenuButton(on: menuOn)
+    }
+
+    func addHamburgerMenuButton (on:Bool) {
+        if hamburgerMenuButton != nil {
+            hamburgerMenuButton?.removeFromSuperview()
+            hamburgerMenuButton = nil
+        }
+        
+        
+        let animation = on ? "buttonOff" : "buttonOn"
+        
+        hamburgerMenuButton = LOTAnimationView(name: animation)
+        hamburgerMenuButton?.isUserInteractionEnabled = true
+        hamburgerMenuButton?.frame = hamburgerMenuFrame
+        hamburgerMenuButton?.contentMode = .scaleAspectFill
+        
+        addMenuToggleRecognizer()
+        
+        self.view.addSubview(hamburgerMenuButton!)
+        
+    }
+    
+    func addMenuToggleRecognizer (){
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.toggleMenu(recognizer:)))
+        
+        tapRecognizer.numberOfTapsRequired = 1
+        
+        hamburgerMenuButton?.addGestureRecognizer(tapRecognizer)
+        
+    }
+    
+    @objc func toggleMenu (recognizer:UITapGestureRecognizer) {
+        
+        if !menuOn {
+            hamburgerMenuButton?.play(completion: { (success:Bool) in
+                self.menuOn = true
+                self.addHamburgerMenuButton(on: self.menuOn)
+            })
+        }else{
+            hamburgerMenuButton?.play(completion: { (success:Bool) in
+                self.menuOn = false
+                self.addHamburgerMenuButton(on: self.menuOn)
+            })
+        }
+        
+    }
+    
+    @IBAction func showAni(_ sender: Any) {
         let aniView2 = LOTAnimationView(name: "3d_rotate_loading_animation")
         aniView2.frame = CGRect(x: 0, y: 0, width: aniView.frame.size.width, height: aniView.frame.size.height)
         aniView2.contentMode = .scaleAspectFit
+        
         aniView2.loopAnimation = true
         self.aniView?.addSubview(aniView2)
         aniView2.play()
-    }
-
-    
-    @IBAction func showAni(_ sender: Any) {
-        
     }
     
     override func didReceiveMemoryWarning() {
