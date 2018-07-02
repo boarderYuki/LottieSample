dbd93f30d7293035c9392a2d2e1f1586b7bd1fca
yuki.pro <yuki.kr@gmail.com>
2018-07-02 23:09:48 +0900

    no message

--- LottieSample/ViewController.swift
+++ LottieSample/ViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import Lottie
+
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var aniView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        let aniView2 = LOTAnimationView(name: "3d_rotate_loading_animation")
+        aniView2.frame = CGRect(x: 0, y: 0, width: aniView.frame.size.width, height: aniView.frame.size.height)
+        aniView2.contentMode = .scaleAspectFit
+        aniView2.loopAnimation = true
+        self.aniView?.addSubview(aniView2)
+        aniView2.play()
     }
 
+    
+    @IBAction func showAni(_ sender: Any) {
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
